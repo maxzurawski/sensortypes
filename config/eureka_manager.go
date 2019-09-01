06fb6183e695872d82989c5c188260a3c72c6b48
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-01 16:42:38 +0200

    Initial version of eureka manager

---
+++ config/eureka_manager.go
@@ -0,0 +1,17 @@
+package config
+
+import "github.com/xdevices/utilities/discovery"
+
+type EurekaManager struct {
+	discovery.Manager
+}
+
+func EurekaManagerInit() *EurekaManager {
+	manager := EurekaManager{
+		Manager: discovery.Manager{
+			RegistrationTicket: Config().RegistrationTicket(),
+			EurekaService:      Config().EurekaService(),
+		},
+	}
+	return &manager
+}
