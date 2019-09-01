ad7a99454d61ebea99ebcb4b28dbe197eb96cf6a
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-01 17:28:18 +0200

    Initial version of DBManager

---
+++ dbprovider/DBManager.go
@@ -0,0 +1,49 @@
+package dbprovider
+
+import (
+	"fmt"
+
+	"github.com/labstack/gommon/log"
+
+	"github.com/jinzhu/gorm"
+	"github.com/xdevices/sensortypes/config"
+	"github.com/xdevices/sensortypes/dto"
+	"github.com/xdevices/sensortypes/model"
+	"github.com/xdevices/utilities/db"
+
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var Mgr DBManager
+
+type DBManager interface {
+	Save(sensorTypeDTO dto.SensorTypeDTO) (*model.SensorType, error)
+	Update(sensorTypeDTO dto.SensorTypeDTO) (*model.SensorType, error)
+	Delete(id uint) (bool, error)
+	GetAll() ([]model.SensorType, error)
+	GetById(id uint) (*model.SensorType, error)
+	GetDb() *gorm.DB
+}
+
+type manager struct {
+	db *gorm.DB
+}
+
+func InitDbManager() {
+	dbPath := config.Config().DBPath()
+
+	if path, exists := db.AdjustDBPath(dbPath); !exists {
+		dbPath = path
+	}
+
+	db2, err := gorm.Open("sqlite3", dbPath)
+
+	if err != nil {
+		erroMsg := fmt.Sprintf("failed to init db[%s]", dbPath)
+		log.Fatal(erroMsg, err)
+	}
+
+	db2.SingularTable(true)
+	db2.AutoMigrate(&model.SensorType{})
+	Mgr = &manager{db: db2}
+}
