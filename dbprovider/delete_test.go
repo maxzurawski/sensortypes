7a29769b9a6f28043809b0a11c720b5e4f0bb8be
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-04 22:11:43 +0200

    Unit tests for delete function

---
+++ dbprovider/delete_test.go
@@ -0,0 +1,65 @@
+package dbprovider
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/xdevices/utilities/db"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// define suite struct for delete
+type DeleteSuiteTest struct {
+	suite.Suite
+}
+
+// initialize our suite object
+func TestDeleteSuiteTest(t *testing.T) {
+	suite.Run(t, new(DeleteSuiteTest))
+}
+
+// setup environment
+func (d *DeleteSuiteTest) SetupTest() {
+	_ = os.Setenv("SERVICE_NAME", "sensortypes")
+	_ = os.Setenv("HTTP_PORT", "8101")
+	_ = os.Setenv("EUREKA_SERVICE", "http://xdevicesdev.home:8761")
+	_ = os.Setenv("DB_PATH", "/Users/l0cke/.databases/xdevices/test/sensortypes.db")
+	InitDbManager()
+}
+
+// failure check -> try to delete not existing sensor type
+func (d *DeleteSuiteTest) TestDelete_Failure() {
+
+	// Arrange & Act.
+
+	result, err := Mgr.Delete(uint(9876))
+
+	// Assert.
+
+	assert.NotNil(d.T(), err)
+	assert.Equal(d.T(), "sensortype not found", err.Error())
+	assert.False(d.T(), result)
+}
+
+// success check -> delete existing sensor type
+func (d *DeleteSuiteTest) TestDelete_Success() {
+
+	// Arrange.
+
+	cleaner := db.DeleteCreatedEntities(Mgr.GetDb())
+	defer cleaner()
+	prepareSensorTypes()
+	sensorType, _ := Mgr.GetByType("TEMPERATURE")
+	assert.NotNil(d.T(), sensorType.ID)
+
+	// Act.
+
+	result, err := Mgr.Delete(*sensorType.ID)
+
+	// Assert.
+
+	assert.Nil(d.T(), err)
+	assert.True(d.T(), result)
+}
