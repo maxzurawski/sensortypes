f5155fecc42151b5f141d8448e6e4f667eb2bd58
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-05 22:49:56 +0200

    Introducing unit tests for update function

---
+++ service/update_test.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/xdevices/sensortypes/dbprovider"
+	"github.com/xdevices/utilities/db"
+)
+
+// suite struct
+type UpdateSuite struct {
+	suite.Suite
+}
+
+// initialize our suite
+func TestUpdateSuite(t *testing.T) {
+	suite.Run(t, new(UpdateSuite))
+}
+
+// provide setup test function
+func (us *UpdateSuite) SetupTest() {
+	environmentPreparations()
+}
+
+// failure check -> version is different then version stored in database
+func (us *UpdateSuite) TestUpdate_Failure() {
+
+	// Arrange.
+
+	cleaner := db.DeleteCreatedEntities(dbprovider.Mgr.GetDb())
+	defer cleaner()
+	sensorTypesData()
+	dtoToUpdate, _ := Service.GetByType("TEMPERATURE")
+	dtoToUpdate.Version = 11
+
+	// Act.
+
+	result, err := Service.Update(*dtoToUpdate)
+
+	// Assert.
+
+	assert.Nil(us.T(), result)
+	assert.NotNil(us.T(), err)
+	assert.Equal(us.T(), "entity versions differs from given input. Please update your state of sensortype and try again", err.Error())
+
+}
+
+// success check -> update sensor type
+func (us *UpdateSuite) TestUpdate_Success() {
+
+	// Arrange.
+
+	cleaner := db.DeleteCreatedEntities(dbprovider.Mgr.GetDb())
+	defer cleaner()
+	sensorTypesData()
+	dtoToUpdate, _ := Service.GetByType("TEMPERATURE")
+	assert.Equal(us.T(), "Temperature sensor", dtoToUpdate.Name)
+
+	dtoToUpdate.Name = "Dummy Temperature sensor"
+
+	// Act.
+
+	result, err := Service.Update(*dtoToUpdate)
+
+	// Assert.
+
+	assert.Nil(us.T(), err)
+	assert.Equal(us.T(), "Dummy Temperature sensor", result.Name)
+	assert.Equal(us.T(), dtoToUpdate.Version+1, result.Version)
+}
