a3d78c33b3273c8650e64262ee73f47120b6d8dc
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-08 17:14:46 +0200

    Delete handler

---
+++ handlers/handle_delete.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/xdevices/sensortypes/service"
+
+	"github.com/labstack/echo"
+)
+
+// NOTE: DELETE /:id
+func HandleDelete(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.NewHTTPError(http.StatusBadRequest, err.Error()))
+	}
+	result, err := service.Service.Delete(uint(id))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.NewHTTPError(http.StatusInternalServerError, err.Error()))
+	}
+	return c.JSON(http.StatusOK, result)
+}
