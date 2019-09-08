31a9074e8945b2744dd7190fea912721183d5748
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-08 18:12:29 +0200

    Initial version of Dockerfile and run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "#############################################"
+echo "Waiting for eureka"
+echo "#############################################"
+
+while ! `nc -z discovery 8761 `; do sleep 3; done
+
+echo "#############################################"
+echo "Waiting for proxy"
+echo "#############################################"
+
+while ! `nc -z proxy 8000 `; do sleep 3; done
+
+echo "#############################################"
+echo "Ready to rumble. Starting sensortypes service"
+echo "#############################################"
+/sensortypes
