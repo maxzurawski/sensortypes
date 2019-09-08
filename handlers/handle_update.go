682ad979ed5a11729ec9f0044cdc970a1bd6fcdc
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-08 15:51:46 +0200

    Handlers update

---
+++ handlers/handle_update.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/xdevices/sensortypes/service"
+
+	"github.com/xdevices/sensortypes/dto"
+
+	"github.com/labstack/echo"
+)
+
+// PUT /:id body: dto.SensorTypeDTO
+func HandleUpdate(c echo.Context) error {
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.NewHTTPError(http.StatusBadRequest, err.Error()))
+	}
+
+	var sensorTypeDTO dto.SensorTypeDTO
+	if err = c.Bind(&sensorTypeDTO); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.NewHTTPError(http.StatusBadRequest, err.Error()))
+	}
+
+	if sensorTypeDTO.ID != uint(id) {
+		return c.JSON(http.StatusConflict, echo.NewHTTPError(http.StatusConflict, fmt.Sprintf("given sensortype cannot be updated under id: %d", id)))
+	}
+
+	resultDTO, err := service.Service.Update(sensorTypeDTO)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.NewHTTPError(http.StatusBadRequest, err.Error()))
+	}
+
+	return c.JSON(http.StatusAccepted, resultDTO)
+}
