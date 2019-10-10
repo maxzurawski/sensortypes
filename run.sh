ccea7f931832fed033374ef1534de0f20a227834
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-10 15:51:04 +0200

    Waiting for rabbit to start

--- run.sh
+++ run.sh
@@ -12,6 +12,11 @@ echo "#############################################"
 
 while ! `nc -z proxy 8000 `; do sleep 3; done
 
+echo "**************************************************************"
+echo "Waiting for the rabbit service to start "
+echo "**************************************************************"
+while ! `nc -z rabbit 15672 `; do sleep 3; done
+
 echo "#############################################"
 echo "Ready to rumble. Starting sensortypes service"
 echo "#############################################"
